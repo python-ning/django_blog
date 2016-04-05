99d4ea4b8900e008eedb5eba78051143d00e0cfc
= <=>
2016-04-05 15:58:43 +0800

    大致跟后端连接完，未实现功能有评论、分页、文章归档等

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
 from models import *
 
+
 def index(request):
     banners = Banner.objects.all()
-    return render(request, 'index.html', {'banners': banners})
+    articles = Article.objects.all()[:4]
+    categorys = Category.objects.all()
+    tags = Tag.objects.all()
+    return render(request, 'index.html', {'banners': banners, 'articles': articles, 'categorys': categorys,
+                                          'tags': tags})
 
 
-def photo(request): 
+def photo(request):
     return render(request, 'photo.html', locals())
 
 
 def article(request):
-    return render(request, 'article.html', locals())
+    articles = Article.objects.all()
+    new_articles = Article.objects.order_by('create_date')
+    categorys = Category.objects.all()
+    tags = Tag.objects.all()
+    return render(request, 'article.html', {'articles': articles, 'categorys': categorys, 'tags': tags, 'new_articles': new_articles})
 
 
 def contact(request):
     return render(request, 'contact.html', locals())
 
-def article_info(request):
+
+def article_info(request, id):
     return render(request, 'article_info.html', locals())
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -5,12 +5,11 @@ from blog.views import *
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^$', photo.views.main, name='index'),
     url(r'^$', index, name='index'),
     url(r'^photo$', photo, name='photo'),
     url(r'^article$', article, name='article'),
     url(r'^contact$', contact, name='contact'),
-    url(r'^article_info$', article_info, name='article_info'),
+    url(r'^article_info/(?P<id>\d+)$', article_info, name='article_info'),
 ]
 
 urlpatterns += patterns('',
